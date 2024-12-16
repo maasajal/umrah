34cffe316cb0dd270640ff504c60f48c72bb9645
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-12-16 20:36:22 +0200

    workflow yml file changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Umrah - BusinessEasy",
   description:
-    "True path of life is Islam. Best & Cheapest Umrah package for all over muslims",
+    "True path of life is Islam. Best & Cheapest Umrah package for all over muslims. Umrah from Finland. Join us for a Spiritual Journey: Umrah in Soudi Arabia. Embark on a life-changing journey to the holy cities of Makkah and Madinah. Join our exclusive 7-days Umrah package and experience the peace, blessings, and spirituality of Umrah with Comfort and ease.",
 };
 
 export default function RootLayout({
