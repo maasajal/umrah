f9a9e0ea716be579fa4dbe1cb3ff5e6d4fbd7fb1
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-12-22 16:15:11 +0200

    Change Navbar and banner

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,6 +9,7 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
 import Link from "next/link";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 
 const navItems = [
   {
@@ -20,7 +21,9 @@ const navItems = [
     path: "packages",
   },
   { name: "Events", path: "events" },
-  { name: "Blogs", path: "blogs" },
+  { name: "Flight", path: "flight" },
+  { name: "Visa", path: "visa" },
+  { name: "Hotel", path: "hotel" },
 ];
 
 const Header = () => {
@@ -35,7 +38,7 @@ const Header = () => {
   };
 
   return (
-    <header className="sticky top-0 left-0 right-0 z-50 bg-[#000000A6]">
+    <header className="sticky top-0 left-0 right-0 z-50 bg-[#000000A6] text-white">
       <div className="container mx-auto px-4 py-1">
         <Toolbar>
           {/* Left side branding */}
@@ -46,7 +49,7 @@ const Header = () => {
             href="/"
             sx={{
               mr: 2,
-              display: { xs: "none", sm: "flex" },
+              display: { xs: "none", md: "flex" },
               fontFamily: "monospace",
               fontWeight: 700,
               letterSpacing: ".3rem",
@@ -61,7 +64,7 @@ const Header = () => {
           {/* This will push the nav items to the right */}
           <Box
             sx={{
-              display: { xs: "none", sm: "flex" },
+              display: { xs: "none", md: "flex" },
               justifyContent: "flex-end",
               flexGrow: 1,
             }}
@@ -75,9 +78,20 @@ const Header = () => {
                 <Link href={page.path}>{page.name}</Link>
               </Button>
             ))}
+            <Button>
+              <Link
+                href="tel:+358449869280"
+                className="bg-blue-600 hover:bg-red-800 text-white text-lg px-5 py-2 rounded-full transition ml-5 flex gap-2 items-center"
+              >
+                <WhatsAppIcon />
+              </Link>
+            </Button>
           </Box>
           {/* Mobile version */}
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", sm: "none" } }}>
+          <Box
+            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+            className="flex items-center justify-between"
+          >
             <IconButton
               size="large"
               aria-label="menu"
@@ -111,26 +125,34 @@ const Header = () => {
                 </MenuItem>
               ))}
             </Menu>
+            {/* Mobile Typography */}
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: "flex", md: "none" },
+                flexGrow: 1,
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              Umrah Support
+            </Typography>
+            <Button>
+              <Link
+                href="tel:+358449869280"
+                className="text-white hover:text-red-600 text-lg px-6 py-2 rounded-full transition"
+              >
+                <WhatsAppIcon />
+              </Link>
+            </Button>
           </Box>
-          {/* Mobile Typography */}
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: "flex", sm: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            Umrah Support
-          </Typography>
         </Toolbar>
       </div>
     </header>
--- src/pages/home/KaabaVideo.tsx
+++ src/pages/home/KaabaVideo.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const KaabaVideo = () => {
   return (
     <>
@@ -12,22 +14,48 @@ const KaabaVideo = () => {
           <source src="/assets/kaabaVideo.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-
+        {/* Overlay */}
+        <div className="absolute inset-0 bg-black bg-opacity-50"></div>
         {/* Content Overlay */}
-        <div className="absolute top-0 left-0 w-full h-96 bg-black bg-opacity-50 flex items-center justify-center text-center">
-          <div className="text-white">
-            <h1 className="text-5xl font-bold text-center py-5">
-              Welcome to Umrah
-            </h1>
-            <p className="text-center max-w-xl mx-auto leading-8">
-              Umrah from Finland. Join us for a Spiritual Journey: Umrah in
-              Soudi Arabia. Embark on a life-changing journey to the holy cities
-              of Makkah and Madinah. Join our exclusive 7-days Umrah package and
-              experience the peace, blessings, and spirituality of Umrah with
-              Comfort and ease.
+        <div className="absolute top-0 left-0 w-full h-96 bg-black bg-opacity-50 flex items-center justify-center text-center text-white">
+          <div className="space-y-5">
+            <h1 className="tracking-widest uppercase">Eid & Umrah in Mekka</h1>
+            <p className="font-semibold">
+              Accommodation: Luxury Hotel Flight | Visa | Transfer & More
             </p>
           </div>
         </div>
+        {/* <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white px-4 space-y-6">
+          <h1 className="tracking-widest uppercase">Eid & Umrah in Mekka</h1>
+          <p className="text-lg sm:text-xl md:text-2xl font-medium">
+            €600 - €800 (All Inclusive)
+          </p>
+          <p className="text-sm sm:text-base md:text-lg font-light uppercase">
+            January, February, March 2025
+          </p>
+          <div className="bg-white text-black py-2 px-4 rounded-lg shadow-lg">
+            <p className="font-semibold">
+              Accommodation: Luxury Hotel <br />
+              Flight | Visa | Transfer & More
+            </p>
+          </div>
+          <p className="text-center max-w-xl mx-auto leading-8">
+            Umrah from Finland. Join us for a Spiritual Journey: Umrah in Soudi
+            Arabia. Embark on a life-changing journey to the holy cities of
+            Makkah and Madinah. Join our exclusive 7-days Umrah package and
+            experience the peace, blessings, and spirituality of Umrah with
+            Comfort and ease.
+          </p>
+          <p className="text-lg sm:text-xl md:text-2xl font-light uppercase">
+            Tour Route: Jeddah, Mekka, Madinah
+          </p>
+          <Link
+            href="tel:+358449869280"
+            className="bg-red-600 hover:bg-red-800 text-lg px-6 py-3 rounded-full shadow-lg transition my-5"
+          >
+            Book Now: +358 449 869280
+          </Link>
+        </div> */}
       </div>
     </>
   );
